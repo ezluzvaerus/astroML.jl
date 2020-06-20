c12e88168c9c7e12e2391aaf287cf4645e560b6b
Paul Hitt <ezluzvaerus@pm.me>
2020-06-19 22:17:19 -0400

    trying out something new with tests

--- ogdon.io.api/bootstrap.jl
+++
@@ -1,7 +0,0 @@
-  cd(@__DIR__)
-  using Pkg
-  pkg"activate ."
-
-  function main()
-    include(joinpath("src", "Ogdon.io.api.jl"))
-  end; main()
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,4 @@ using .SumOfNorms
 using .astroML
 
 @test hello("Julia") == "Hello, Julia"
-@test sinegauss(3,1,2,2) ≈ 9.045238274381776e-6
-@test sinegauss_PSD(3,1,2,2) ≈ 1.8709943196961264e118
 @test norm(1,2,3) ≈ 0.13710326745732856
-@test ssinegauss_FT(3,1,2,2) ≈ 1.266905008632257e7
